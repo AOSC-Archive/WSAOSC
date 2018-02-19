2591cd533f870ec86adb29fb644f0815f6f96ce0
Richard Yu <yurichard3839@gmail.com>
2018-02-19 18:41:14 +0800

    main: run wsl.

--- main.c
+++ main.c
@@ -6,6 +6,12 @@
 #include <wchar.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <stdlib.h>
+
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
+#endif /* _MSC_VER */
 
 #include "wslutil.h"
 
@@ -52,6 +58,39 @@ bool install_distor(void)
 	return false;
 }
 
+bool run_wsl(int argc, wchar_t *argv[])
+{
+	size_t command_len = 0;
+	for (int i = 0; i < argc; i++)
+		command_len += wcslen(argv[i]) + 1;
+
+	wchar_t *command_line = NULL;
+	if (command_len != 0)
+	{
+		command_line = (wchar_t*)malloc(command_len * sizeof(wchar_t));
+		if (!command_line)
+			return false;
+		command_line[0] = L'\0';
+
+		for (int i = 0; i < argc; i++)
+		{
+			wcscat(command_line, argv[i]);
+			size_t len = wcslen(command_line);
+
+			if (i < (argc - 1))
+			{
+				command_line[len] = L' ';
+				command_line[len + 1] = L'\0';
+			}
+		}
+		command_line[command_len - 1] = L'\0';
+	}
+
+	DWORD exit_code;
+	HRESULT hr = _WslLaunchInteractive(DISTOR_NAME, command_line, TRUE, &exit_code);
+	return SUCCEEDED(hr);
+}
+
 int main(void)
 {
 	if (!load_wsl_api())
@@ -81,13 +120,16 @@ int main(void)
 
 		if (argc < 2)
 		{
-			// run without args
+			if (!run_wsl(0, NULL))
+				retval = 1;
 			break;
 		}
 
 		wchar_t *command = argv[1];
 		if (wcscmp(command, L"run") == 0 || wcscmp(command, L"exec") == 0)
 		{
+			if (!run_wsl(argc - 2, argv + 2))
+				retval = 1;
 		}
 		else if (wcscmp(command, L"config") == 0)
 		{
@@ -100,7 +142,6 @@ int main(void)
 			print_help();
 			retval = 1;
 		}
-
 	} while (0);
 
 	LocalFree(argv);
